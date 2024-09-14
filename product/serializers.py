cf276d9d512454cb1f838d068c315f5fcde888ed
Qazal <ghazalteen28@gmail.com>
2024-09-15 00:18:41 +0330

    final commit and all

--- product/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ProductInfo
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=ProductInfo
-        fields=['id','name', 'description','price', 'image', 'category']
-
